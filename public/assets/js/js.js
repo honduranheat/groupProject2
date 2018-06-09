c7c661eac3a4e4b9f5af16ab57b5c3ba02364c45
Justin <jwhittington92@yahoo.com>
2018-06-09 01:35:46 -0400

    added picture and recipe link/clear elements

--- public/assets/js/js.js
+++ public/assets/js/js.js
@@ -1,9 +1,3 @@
-// $(document).on("click", ".recipeBtn", function() {
-
-// })
-
-
-
 $(document).on("click", ".recipeBtn", function () {
 
 
@@ -11,26 +5,42 @@ $(document).on("click", ".recipeBtn", function () {
         var ingredient = document.getElementById("ingredientSearch").value;
         var dishName = document.getElementById("dishName").value;
         var healthLabel = document.getElementById("healthLabel").value;
+        var appId2 = "5b1562b5";
+        var apiKey2 = "bbb4077c8c6cf7f2526a1bef3aef09c1";
         var apiKey = "d8e8948c4d70b8c78827596832285f57";
         var appId = "ef5218c7";
         console.log("what");
 
-        var url = `https://api.edamam.com/search?app_id=${appId}&app_key=${apiKey}&label=${dishName}&q=${ingredient}&healthLabels=${healthLabel}&count=3&hits=3from=0&to=3`;
+        var url = `https://api.edamam.com/search?app_id=${appId2}&app_key=${apiKey2}&label=${dishName}&q=${ingredient}&healthLabels=${healthLabel}&count=3&from=0&to=4`;
         $.ajax({
             url: url,
             method: "GET",
         }).then(function (data) {
-            console.log(data.hits[0].recipe.label);
-            var lotto = Math.floor(Math.random() * 3);
-            $('#recipeTitle').text(JSON.stringify(data.hits[lotto].recipe.label));
+            //console.log(data.hits[0].recipe.label);
+            $('#recipeTitle').empty();
+            $('#recipeImg').empty();
 
-            console.log(data);
-
+            showRecipe(data);
         });
+    }
 
+    function showRecipe(data) {
 
+        var lotto = Math.floor(Math.random() * 5);
+        var recipeImg = data.hits[lotto].recipe.image;
+        var recipeLink = data.hits[lotto].recipe.shareAs;
 
+        $('#recipeTitle').text(JSON.stringify(data.hits[lotto].recipe.label));
+        $('#recipeImg').append(`<a href="${recipeLink}"><img src=${recipeImg}></a>`);
+        // $('#recipeIngredients').text(JSON.stringify(data.hits[lotto].recipe.ingredientLines));
 
+        console.log(recipeImg);
+        console.log(data);
     }
     getRecipe();
 });
+
+
+// - api is working and info is appearing on main page. feel free to mess with it. 
+// - in index.html i added some divs for the recipe order. 
+// - it needs a better way to get a random result from the JSON data. every so often I get an error. but the recipe is randomizing. change the 'hit' parameter to get more results
